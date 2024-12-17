b0e8724a6026f9f66100d748b96e70ddfa95233b
jebonach <bonnie.2.fnaf@gmail.com>
2024-12-17 18:42:09 +0300

    Final?

--- build/CMakeFiles/program.dir/sort.c.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/program.dir/sort.c.o: /home/jebonach/c-cpp/laba3/sort.c \
- /usr/include/stdc-predef.h /home/jebonach/c-cpp/laba3/sort.h \
- /home/jebonach/c-cpp/laba3/list.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stddef.h \
- /home/jebonach/c-cpp/laba3/publication.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/14.2.1/include/stdbool.h
